0d448dbccbf4c84241c733fa5327d18ae6d8902b
Alex Yu <alexcwyu@gmail.com>
2018-10-11 01:38:54 +0800

    poc grpc

---
+++ grpc-spark/src/main/java/net/alexcwyu/grpc/ReportClient.java
@@ -0,0 +1,72 @@
+package net.alexcwyu.grpc;
+
+
+import com.google.common.collect.Lists;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import net.alexyu.grpc.gen.ReportRequest;
+import net.alexyu.grpc.gen.ReportResponse;
+import net.alexyu.grpc.gen.ReportServiceGrpc;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A simple client that requests a greeting from the {@link ReportServer}.
+ */
+public class ReportClient {
+    private static final Logger logger = Logger.getLogger(ReportClient.class.getName());
+
+    private final ManagedChannel channel;
+    private final ReportServiceGrpc.ReportServiceBlockingStub blockingStub;
+
+    /** Construct client connecting to HelloWorld server at {@code host:port}. */
+    public ReportClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build());
+    }
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    ReportClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = ReportServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /** Say hello to server. */
+    public void query(String id) {
+        logger.info("Will try to greet " + id + " ...");
+        ReportRequest request = ReportRequest.newBuilder().setRequestId(id)
+                .addAllColumns(Lists.newArrayList("delta", "gamma", "tv", "rho"))
+                .addAllGroupBy(Lists.newArrayList("underlying")).build();
+        ReportResponse response;
+        try {
+            response = blockingStub.query(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Report: " + response);
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting.
+     */
+    public static void main(String[] args) throws Exception {
+        ReportClient client = new ReportClient("localhost", 50051);
+        try {
+            client.query("test");
+        } finally {
+            client.shutdown();
+        }
+    }
+}
---
+++ grpc-spark/src/main/java/net/alexcwyu/grpc/ReportServer.java
@@ -0,0 +1,93 @@
+package net.alexcwyu.grpc;
+
+import com.google.common.collect.Lists;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import net.alexyu.grpc.gen.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
+
+/**
+ * Server that manages startup/shutdown of a {@code Greeter} server.
+ */
+public class ReportServer {
+    private static final Logger logger = Logger.getLogger(ReportServer.class.getName());
+
+    private Server server;
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new ReportServiceImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                ReportServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    /**
+     * Main launches the server from the command line.
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final ReportServer server = new ReportServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    static class ReportServiceImpl extends ReportServiceGrpc.ReportServiceImplBase {
+        Random r = new Random();
+        @Override
+        public void query(ReportRequest request, StreamObserver<ReportResponse> responseObserver) {
+
+            ReportResponse.Builder builder = ReportResponse.newBuilder().setRequestId(request.getRequestId());
+
+            int count = r.nextInt(10) + 10;
+
+            List<Record> recordList = Lists.newArrayList();
+            for (int i = 0 ; i < count ; i++) {
+                Record.Builder record = Record.newBuilder();
+                for (String column : request.getColumnsList()) {
+                    record.putFields(column, Value.newBuilder().setDoubleValue(r.nextInt(10) + r.nextDouble()).build());
+                }
+                recordList.add(record.build());
+            }
+            builder.addAllRecords(recordList);
+            responseObserver.onNext(builder.build());
+            responseObserver.onCompleted();
+        }
+
+        //https://github.com/grpc/grpc-java/blob/master/examples/src/main/java/io/grpc/examples/manualflowcontrol/ManualFlowControlServer.java
+        @Override
+        public StreamObserver<ReportRequest> subscribe(StreamObserver<ReportResponse> responseObserver) {
+            return super.subscribe(responseObserver);
+        }
+    }
+}
